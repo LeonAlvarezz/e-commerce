4c025943336109dfda5582197e5215e93f09eced
LeonAlvarezz <107019128+LeonAlvarezz@users.noreply.github.com>
2023-09-23 10:08:50 +0700

    Add Product Image

--- components/product.tsx
+++ components/product.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import products from '@/mock_data/product';
+import Image from 'next/image';
 export default function Product() {
   return (
-    <div className='text-2xl'>
-      <ul>
+    <div className='m-auto w-[95%]'>
+      <ul className='grid grid-cols-5'>
         {products.map((product) => (
           <li key={product.key}>
-            <h1>{product.name}</h1>
-            <h2>{product.price}</h2>
+            <div className='ml-10 grid h-[338px] w-[427px] place-items-center bg-gray-200'>
+              <div className='w-[250px]'>
+                <Image
+                  src={product.image}
+                  width={0}
+                  height={0}
+                  layout='responsive'
+                  alt='product'
+                />
+              </div>
+            </div>
+            <div>
+              <h1>{product.name}</h1>
+            </div>
           </li>
         ))}
       </ul>
--- mock_data/product.ts
+++ mock_data/product.ts
@@ -1,35 +1,38 @@
 type Product = {
-    id: number,
-    name: string,
-    des: string,
-    price: number,
-}
+  id: number;
+  name: string;
+  des: string;
+  price: number;
+  image: string;
+};
 
 const products: Product[] = [
-    {
-        id: 1,
-        name: "Random Shoe",
-        des: "Random Shoe from Random Brands",
-        price: 200
-    },
-    {
-        id: 2,
-        name: "Random Clothes",
-        des: "Random Clothes from Random Brands",
-        price: 200
-    },
-    {
-        id: 3,
-        name: "Random Jacket",
-        des: "Random Jacket from Random Brands",
-        price: 200
-    },
-    {
-        id: 4,
-        name: "Random Pant",
-        des: "Random Pant from Random Brands",
-        price: 200
-    },
-]
+  {
+    id: 1,
+    name: 'Random Shoe',
+    des: 'Random Shoe from Random Brands',
+    price: 200,
+    image: '/img/product/shoe1.png',
+  },
+  {
+    id: 2,
+    name: 'Random Clothes',
+    des: 'Random Clothes from Random Brands',
+    price: 200,
+    image: '/img/product/clothes1.png',
+  },
+  {
+    id: 3,
+    name: 'Random Jacket',
+    des: 'Random Jacket from Random Brands',
+    price: 200,
+  },
+  {
+    id: 4,
+    name: 'Random Pant',
+    des: 'Random Pant from Random Brands',
+    price: 200,
+  },
+];
 
 export default products;
